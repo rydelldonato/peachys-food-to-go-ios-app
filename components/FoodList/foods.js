20e1ac4b627d51e24803ab75431295a87ed80480
Rydell <donatorydell98@gmail.com>
2023-03-28 13:42:05 -0700

    added snap scroll effect and food images

--- components/FoodList/foods.js
+++ components/FoodList/foods.js
@@ -1,12 +1,12 @@
 export default [{
     name: "COMBO #2",
     price: "Starting from $15.99",
-    image: require("../../assets/images/foodKamayan.jpg"),
+    image: require("../../assets/images/combo2.png"),
     info: "Sisig over Garlic Rice with Lumpia and Water"
 },{
     name: "Sisig over Garlic Rice",
     price: "Starting from $11.99",
-    image: require("../../assets/images/foodKamayan.jpg"),
+    image: require("../../assets/images/garlicOverRice.png"),
     info: "Garlic rice topped with pork or chicken"
 },{
     name: "Crispy Sisig Street Tacos",
@@ -16,7 +16,7 @@ export default [{
 },{
     name: "Pork Belly Filipino Asado Bao",
     price: "Starting from $9.99",
-    image: require("../../assets/images/foodKamayan.jpg"),
+    image: require("../../assets/images/porkBelly.png"),
     info: "Steamed sweet dough bun stuffed with Pork Belly Filipino style Asado"
 },{
     name: "Sisig Fries",
@@ -26,22 +26,22 @@ export default [{
 },{
     name: "Shanghai Lumpia",
     price: "Starting from $6.99",
-    image: require("../../assets/images/foodKamayan.jpg"),
+    image: require("../../assets/images/shanghaiLumpia.png"),
     info: "(10pcs)"
 },{
     name: "Deep Fried Banana Fritters",
     price: "Starting from $4.99",
-    image: require("../../assets/images/foodKamayan.jpg"),
+    image: require("../../assets/images/bananaFritters.png"),
     info: "with Toasted Sesame Seeds & Orange Liquer Dipping Sauce"
 },{
     name: "Tofu Asado Bao",
     price: "Starting from $8.99",
-    image: require("../../assets/images/foodKamayan.jpg"),
+    image: require("../../assets/images/sisigBao.png"),
     info: "Steamed sweet dough bun stuffed with Filipino Asado flavored Tofu"
 },{
     name: "Loco Moco Lumpia",
     price: "Starting from $8.99",
-    image: require("../../assets/images/foodKamayan.jpg"),
+    image: require("../../assets/images/locoMocoLumpia.png"),
     info: "(2pcs)"
 }
 ]
--- components/FoodList/index.js
+++ components/FoodList/index.js
@@ -1,13 +1,14 @@
-import { View ,FlatList } from 'react-native';
+import { View ,FlatList, Dimensions } from 'react-native';
 import React from 'react';
-import FoodItem from '../foodItem/index.js';
+import FoodItem from '../FoodItem/index.js';
 import styles from './styles';
 import foods from './foods';
+
 
 const FoodList = (props) => {
   return (
     <View style={styles.container}>
-      <FlatList data={foods} renderItem={({item}) => <FoodItem food={item} /> } />
+      <FlatList data={foods} renderItem={({item}) => <FoodItem food={item} /> } snapToAlignment={"start"} decelerationRate={"fast"} snapToInterval={Dimensions.get("window").height} />
     </View>
   )
 }
